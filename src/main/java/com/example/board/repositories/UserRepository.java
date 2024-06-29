67f122ea3c3294824a1e57a0431f6f22e1ca8316
sicd <julonfiv@gmail.com>
2024-06-29 01:24:43 -0700

    fix typo that makes build error

--- src/main/java/com/example/board/repositories/UserRepository.java
+++ src/main/java/com/example/board/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    boolean existByUsername(String username);
+    boolean existsByUsername(String username);
 }
