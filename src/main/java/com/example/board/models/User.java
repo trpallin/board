1c09330e437ca7e99f61062dee98406a359b8af5
sicd <julonfiv@gmail.com>
2024-06-29 01:34:58 -0700

    introduce lombok

--- src/main/java/com/example/board/models/User.java
+++ src/main/java/com/example/board/models/User.java
@@ -1,9 +1,13 @@
 package com.example.board.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,28 +18,4 @@ public class User {
 
     @Column(nullable = false)
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
